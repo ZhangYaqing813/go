a823a5e31dbee5fdd7b12f80b4e45882cdca09c3
zhangyaqing <zhangyaqing59@126.com>
2020-01-29 22:36:01 +0800

    tijiao

--- suanfa/main.go
+++ suanfa/main.go
@@ -1,19 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"zyq.cn/suanfa/untils"
+)
 
 func main() {
 	var arrv[10] int = [...] int {10, 9, 12, 6, 8, 1, 90, 18, 20, 28}
-	var lenth = len(arrv) - 1
-	for i := 0; i <lenth ; i++{
-		for j :=lenth; j>i; j--{
-			if arrv[j] < arrv[j-1]{
-				//tmp := arrv[j]
-				//arrv[j] = arrv[j-1]
-				//arrv[j-1] = tmp
-				arrv[j], arrv[j-1] = arrv[j-1], arrv[j]
-			}
-		}
-	}
+	untils.Maopao(&arrv)
+	fmt.Println()
 	fmt.Println(arrv)
+	fmt.Println()
+	untils.Binary(arrv,20)
+
 }
--- suanfa/untils/untils.go
+++ suanfa/untils/untils.go
@@ -1,5 +1,46 @@
-package main
+package untils
 
-func main() {
-	
+import (
+	"fmt"
+)
+
+func Maopao( arrv *[10]int ) {
+
+	var lenth = len(arrv) - 1
+	for i := 0; i <lenth ; i++{
+		for j :=lenth; j>i; j--{
+			if arrv[j] < arrv[j-1]{
+				//tmp := arrv[j]
+				//arrv[j] = arrv[j-1]
+				//arrv[j-1] = tmp
+				arrv[j], arrv[j-1] = arrv[j-1], arrv[j]
+			}
+		}
+	}
+	fmt.Println(*arrv)
+}
+
+func Binary( arrv2[10]int, num int)  {
+	var left int = 0
+	var right int = len(arrv2) - 1
+	for left <= right{
+		var mid = left +(right - left) / 2
+		fmt.Println("middle index is ", mid)
+		fmt.Println("minddle value is ",arrv2[mid])
+
+		if arrv2[mid] == num {
+			fmt.Println("find the num ", arrv2[mid])
+			break
+		} else if arrv2[mid] > num{
+			right = mid -1
+
+			fmt.Println("right index is ", right)
+			fmt.Println("right value is ", arrv2[right])
+		}else if arrv2[mid] < num {
+			left = mid + 1
+
+		}
+
+	}
+
 }
