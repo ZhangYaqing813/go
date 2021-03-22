1bea8247d7d3952a2ad4c5a75ef6dd733b2d73e6
ZhangYaqing <zhangyaqing59@126.com>
2021-03-22 18:06:45 +0800

    "xin"

--- arrv/main.go
+++ arrv/main.go
@@ -1,6 +1,8 @@
 package main
 
-import arrv "go/arrv/func"
+import (
+	"fmt"
+)
 
 func main() {
 	//var test [26] byte
@@ -27,16 +29,38 @@ func main() {
 	//fmt.Printf("最大数：%d, 索引是：%d", maxVal, maxIdex)
 
 	/*
-	var arr [10] int
-	for i := 0; i<10; i++{
-		time.Sleep(10)
-		(&arr)[i] = arrv.RandNumber()
-	}
-	arrv.Arrex1(&arr)
-	fmt.Println("\n=======")
+		var arr [10] int
+		for i := 0; i<10; i++{
+			time.Sleep(10)
+			(&arr)[i] = arrv.RandNumber()
+		}
+		arrv.Arrex1(&arr)
+		fmt.Println("\n=======")
 
-	var arr2[5] int = [5]int {1,3,5,7,9}
-	arrv.InsertNumber(&arr2,4)
-*/
-	arrv.Towarr()
+		var arr2[5] int = [5]int {1,3,5,7,9}
+		arrv.InsertNumber(&arr2,4)
+	*/
+	//arrv.Towarr()
+
+	q := [4]int{1, 2, 3, 4}
+	fmt.Println("q=", q)
+
+	for i := 0; i <= len(q)-1; i++ {
+		fmt.Println(q[i])
+	}
+
+	for index := 0; index < len(q); index++ {
+		value := q[index]
+		fmt.Printf("index= %d vaule= %d\n", index, value)
+	}
+
+	var qq [4][4]int
+	for x := 0; x < 4; x++ {
+		for y := 0; y < 4; y++ {
+			qq[x][y] = x*y + 1
+		}
+		fmt.Println("qq[x]= ", qq[x])
+	}
+	fmt.Println("\n qq=", qq)
+
 }
