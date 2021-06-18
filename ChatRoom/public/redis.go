93c363ec95471a94db5bb4b484ff83e509089ff8
ZhangYaqing <zhangyaqing59@126.com>
2021-06-18 17:53:54 +0800

    redis 验证登录

--- ChatRoom/public/redis.go
+++ ChatRoom/public/redis.go
@@ -19,19 +19,18 @@ func redisCon() (pool *redis.Pool) {
 	}
 }
 
-func Push() {
-	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		fmt.Println("连接失败 err = ", err)
-	}
-
+func RedisPush(userID int, userPwd string) (code int) {
+	conn := redisCon().Get()
 	defer conn.Close()
-
-	_, err = conn.Do("set", 100, "abc")
+	_, err := conn.Do("set", userID, userPwd)
 	if err != nil {
-		fmt.Println("set key err = ", err)
+		fmt.Println("写入redis err= ", err)
+		code = 500
+	} else {
+		code = 200
 	}
 
+	return code
 }
 
 func RedisGet(userID int, userPwd string) (code int) {
